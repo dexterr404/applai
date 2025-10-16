85124191b59a3d33facf17b2e0387fef47ee5aab
Dexter Javier <dexterjavier543@gmail.com>
2025-10-16 21:59:26 +0800

    fix(bug): railway deployement fix of name

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 
-import jobRoutes from "./routes/tempRoute.ts"
+import jobRoutes from "./routes/jobRoutes.ts"
 import authRoutes from "./routes/authRoutes.ts"
 import aiRoutes from "./routes/aiRoutes.ts"
 
