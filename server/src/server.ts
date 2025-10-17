fbf8f33896a581afd02dedc0f9df4412f881932a
Dexter Javier <dexterjavier543@gmail.com>
2025-10-17 17:40:54 +0800

    fix(cors): try to fix redirect uri mismatch

--- server/src/server.ts
+++ server/src/server.ts
@@ -19,7 +19,6 @@ app.use(cors({
         process.env.APP_BASE_URL!,
         "https://applai-seven.vercel.app"
     ],
-    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
